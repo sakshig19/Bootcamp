c97e074be74284c3fe9798d4c9ba1728835df6b2
sakshig19 <126548290+sakshig19@users.noreply.github.com>
2025-02-07 10:26:39 +0530

    Delete PosNegZero.c

--- PosNegZero.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    // TO find if the number is positive/negative/zero
-    int a,ans;
-    printf("Enter the number: ");
-    scanf("%d",&a);
-    ans= (a==0)?((a<0)?printf("Negative"):printf("Zero")):((a>0)?printf("Positive"):printf("Negative"));
-    return 0;
-}
